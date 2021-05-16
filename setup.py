3e274775c96ffb5ed93925f9fd7fe6a901b135bc
Carlos Pinzón <caph1993@gmail.com>
2021-05-17 00:07:08 +0200

    Update .gitignore, setup.py, and 2 more files...

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-
 '''
 To upload:
 python3 setup.py check
@@ -9,17 +8,17 @@ twine upload dist/*
 
 setup(
     name='caph1993-pytools',
-    version='0.1.0',
+    version='0.1.1',
     description='Python toolbox of Carlos Pinzón',
     url='https://github.com/caph1993/caph1993-pytools',
     author='Carlos Pinzón',
     author_email='caph1993@gmail.com',
     license='MIT',
     packages=[
-    	'custom_json',
+        'custom_json',
+        'strings',
     ],
-    install_requires=[
-    ],
+    install_requires=[],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
