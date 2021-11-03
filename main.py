4b9adf8f9941ead3cdcdafed9121796c8add8693
Carlos Pinzón <caph1993@gmail.com>
2021-11-03 20:00:15 +0100

    checkpoint

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from cp93pytools.audio import Sequence
 from cp93pytools.methodtools import cached_property
-from cp93pytools.process import MyProcess, test
+from cp93pytools.process import MyProcess  #, test
+from cp93pytools.easySQLite import test as test_easySQLite
 
-test()
-
-p = MyProcess([
-    'python3', '-c', 'import time;[print(time.sleep(0.1)) for i in range(300)]'
-])
-
-p.run(capture_stdout=True)
+#test()
+test_easySQLite()
