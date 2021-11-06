56f0e28e02548a54211c429caeae8a458958616e
Carlos Pinzón <caph1993@gmail.com>
2021-11-06 19:14:05 +0100

    v0.2.13 of caph1993-pytools

--- cp93pytools/process.py
+++ cp93pytools/process.py
@@ -73,7 +73,7 @@ class MyProcess():
         self.returncode: int, exit code of the process
     '''
 
-    def __init__(self, args, shell=False, env=None):
+    def __init__(self, args, shell=False, env=None, cwd=None):
         kwargs = locals()
         kwargs.pop('self')
         self.kwargs = Dict(kwargs)
@@ -225,6 +225,7 @@ class MyProcess():
                 #bufsize=1,
                 close_fds=ON_POSIX,
                 env=self.kwargs.env,
+                cwd=self.kwargs.cwd,
             )
         except FileNotFoundError as e:
             self._stop = True
